6078bfca8f7acff4ba406f578643475903fca9d4
Belen Antunez <161779710+BelenAntunez2024@users.noreply.github.com>
2026-09-14 17:46:05 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8000",
